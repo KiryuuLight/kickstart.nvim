68a372c08f7ebee4bb60b11d3671bd9d66f56ebf
Luis Ortiz <70501251+KiryuuLight@users.noreply.github.com>
2024-11-02 14:11:33 -0500

    chore: update keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -16,15 +16,16 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+-- Moving on insert mode
+vim.keymap.set('i', '<C-b>', '<ESC>^i', { desc = 'Move beginning of line' })
+vim.keymap.set('i', '<C-e>', '<End>', { desc = 'Move end of line' })
+
 -- Disable arrow keys in normal mode
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
 --  See `:help wincmd` for a list of all window commands
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
@@ -47,8 +48,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- jq on exit
 vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Escape using j and k' })
-vim.keymap.set('i', '<C-h>', '<C-o>b', { desc = 'Move left by word in insert mode' })
-vim.keymap.set('i', '<C-l>', '<C-o>w', { desc = 'Move right by word in insert mode' })
 -- Open parent directory for oil
 vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 
@@ -57,7 +56,8 @@ vim.keymap.set('v', 'K', ":m '<-2<CR>gv==jgvo<esc>=jgvo", { desc = 'Move highlig
 vim.keymap.set('n', '<cr>', 'ciw', { desc = 'Removed current text on new insert' })
 
 -- Obsidian
-vim.keymap.set('n', '<leader>oc', "<cmd>lua require('obsidian').util.toggle_checkbox()<CR>", { desc = 'Obsidian Check Checkbox' })
+vim.keymap.set('n', '<leader>oc', "<cmd>lua require('obsidian').util.toggle_checkbox()<CR>",
+  { desc = 'Obsidian Check Checkbox' })
 vim.keymap.set('n', '<leader>ot', '<cmd>ObsidianTemplate<CR>', { desc = 'Insert Obsidian Template' })
 vim.keymap.set('n', '<leader>od', '<cmd>ObsidianToday<CR>', { desc = 'Creates daily note' })
 vim.keymap.set('n', '<leader>oo', '<cmd>ObsidianOpen<CR>', { desc = 'Open in Obsidian App' })
