e11e99aa918acc849cb821ef992964d7595acfd8
Luis Ortiz <70501251+KiryuuLight@users.noreply.github.com>
2024-11-03 17:40:58 -0500

    chore: add more filters to noice plugin

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -15,13 +15,15 @@ return {
         level = 'ERROR',
       },
       routes = {
-        {
-          filter = { find = 'written' },
-          opts = { skip = true },
-        },
+        { filter = { event = "msg_show", find = "%d+B written$" },              view = "mini" },
+        { filter = { event = "msg_show", find = "%d+L, %d+B$" },                view = "mini" },
+        { filter = { event = "msg_show", find = "%-%-No lines in buffer%-%-" }, view = "mini" },
+        { filter = { event = "msg_show", find = "change" },                     view = "mini" },
+        { filter = { event = "msg_show", find = "more line" },                  view = "mini" },
+        { filter = { event = "msg_show", find = "fewer lines" },                view = "mini" },
+        { filter = { event = "msg_show", find = "line less" },                  view = "mini" },
       },
       lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
           ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
           ['vim.lsp.util.stylize_markdown'] = true,
@@ -29,11 +31,11 @@ return {
         },
       },
       presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
+        bottom_search = false,        -- use a classic bottom cmdline for search
+        command_palette = true,       -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
+        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false,       -- add a border to hover docs and signature help
       },
     }
   end,
